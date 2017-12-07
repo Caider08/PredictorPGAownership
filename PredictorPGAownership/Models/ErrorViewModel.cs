55cc14e90cf2b131b4e9c1ec64f6c7e3efd577b7
Caider08 <caidbuch@ksu.edu>
2017-12-06 23:28:38 -0600

    Add project files.

---
+++ PredictorPGAownership/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PredictorPGAownership.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
